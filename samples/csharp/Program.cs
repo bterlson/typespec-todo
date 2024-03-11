137582a43dc1ef35a40eba4b4a515bddad793e98
Jesse Squire <jesse.squire@gmail.com>
2024-03-11 17:20:06 -0400

    Adding C# sample, though the generated code does not compile

---
+++ samples/csharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.ClientModel;
+using Todo;
+using Todo.Models;
+
+TodoClient client = new TodoClient(new KeyCredential("token"));
+TodoItems itemsClient = client.GetItemsClient();
+
+Result<TodoItem> result = await itemsClient.CreateAsync(
+    item: new TodoItem("title", TodoItemStatus.NotStarted)
+    {
+        Labels = BinaryData.FromObjectAsJson(new string[] { "label1", "label2" })
+    },
+    attachments:
+    [
+        BinaryData.FromObjectAsJson(new { Url = "https://example.com", Description = "example" })
+    ]
+);
+
+if (result.Value != null)
+{
+    Console.WriteLine($"Created item with id: {result.Value.Id}");
+}
